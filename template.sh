72925e4e0e8b6d97c99d3d3b65253339d6805e06
Saksham8888 <sakshamrawat55555@gmail.com>
2026-06-15 15:03:04 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src 
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/model.py
+touch .env
+
+touch setup.py
+touch app.py
+touch research/trails.py
+touch requirements.txt
+
+echo "project successfully completed!"
